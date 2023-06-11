0cbad68f19cbe656f779b1df77c54709c546828d
KarolPrusek <karol@DESKTOP-B5Q0PDK>
2023-06-11 14:33:53 +0200

    JSON_part_2/11.06.2023

--- src/App/Exercises/Js/SavedInput/SavedInput.jsx
+++ src/App/Exercises/Js/SavedInput/SavedInput.jsx
@@ -1,13 +1,129 @@
+// import './styles.css';
+// import React, { useState, useRef } from 'react';
+
+// export function SavedInput() {
+//   const [list, setList] = useState([]);
+//   const inputRef = useRef(null);
+
+//   const handleAddNick = () => {
+//     const newNick = inputRef.current.value;
+//     if (newNick) {
+//       const newId = list.length + 1;
+//       const newObject = {
+//         nick: newNick,
+//         id: newId,
+//       };
+//       setList((prevList) => [...prevList, newObject]);
+//       inputRef.current.value = '';
+//     }
+//   };
+
+//   const handleRemoveNick = (id) => {
+//     setList((prevList) => prevList.filter((item) => item.id !== id));
+//   };
+
+//   return (
+//     <div className="mainClass">
+//       <div className="nick">NICK</div>
+//       <input className="input" placeholder="nick" type="text" ref={inputRef} />
+
+//       <button className="buttonAdd" onClick={handleAddNick}>
+//         DODAJ
+//       </button>
+//       <div className="nickList">
+//         {list.map((item) => (
+//           <div key={item.id} className="nickItem">
+//             {item.nick}
+//             <button
+//               className="removeButton"
+//               onClick={() => handleRemoveNick(item.id)}
+//             >
+//               X
+//             </button>
+//           </div>
+//         ))}
+//       </div>
+//     </div>
+//   );
+// }
+// ------------------------NOWY KOD------------------------------
 import './styles.css';
+import { useState } from 'react';
+
+const initialListNick = JSON.parse(localStorage.getItem('list-nicki'));
+const initialIdCounter = localStorage.getItem('idCounter');
 
 export function SavedInput() {
+  const [listNick, setListNick] = useState(
+    initialListNick ? initialListNick : []
+  );
+  const [currentName, setCurrentName] = useState('');
+  const [idCounter, setIdCounter] = useState(
+    initialIdCounter && Number(initialIdCounter)
+  );
+
+  const sendToLocalStorage = (newList, count) => {
+    const stringifyNickList = JSON.stringify(newList);
+
+    localStorage.setItem('list-nicki', stringifyNickList);
+    localStorage.setItem('idCounter', count);
+
+    setIdCounter(count);
+    setListNick(newList);
+  };
+
+  const addPerson = (e) => {
+    e.preventDefault();
+    const inputNickValue = currentName;
+    const newList = [...listNick, { nick: inputNickValue }];
+    const count = idCounter + 1;
+
+    sendToLocalStorage(newList, count);
+  };
+
+  const changeValue = (e) => {
+    setCurrentName(e.target.value);
+  };
+
+  const deletePerson = (index) => {
+    const newList = listNick.filter((_, idx) => idx !== index);
+    setListNick(newList);
+    sendToLocalStorage(newList, idCounter);
+  };
+
   return (
-    <div>
-      <div className="nick">
-        NICK
-        <input className="input"></input>
+    <div className="saved-input-container">
+      <form onSubmit={addPerson}>
+        <div className="left-part">
+          <div className="nick-input-row">
+            <p>NICK</p>
+            <input
+              type="text"
+              className="input-nick"
+              placeholder="Podaj swój nick"
+              autoFocus
+              onChange={changeValue}
+            />
+          </div>
+          <button type="submit">DODAJ</button>
+        </div>
+      </form>
+      <div className="list">
+        {listNick &&
+          listNick.map((person, index) => {
+            return (
+              <div className="row-on-list">
+                <div className="name-on-list">{person.nick}</div>
+                <button
+                  className="x-button"
+                  onClick={() => deletePerson(index)}
+                >
+                  X
+                </button>
+              </div>
+            );
+          })}
       </div>
-      <button className="buttonAdd">DODAJ</button>
     </div>
   );
 }
