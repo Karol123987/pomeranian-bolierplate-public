33e2e466ea87f87e83d21319f7164b062cabc9d8
KarolPrusek <karol@DESKTOP-B5Q0PDK>
2023-06-11 15:54:11 +0200

    OPP/11.06.2023

---
+++ src/App/Exercises/Js/exercise-oop/router-data.jsx
@@ -0,0 +1,10 @@
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'oop-js',
+  date: null,
+  linkLabel: 'OOP w JS',
+  blockNo: 26,
+  element: <Exercise />,
+  tags: ['js', 'map', 'oop', 'classes'],
+};
