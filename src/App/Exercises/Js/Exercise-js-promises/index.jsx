dc0ca5954321cd203d074e9063091ac992de2814
KarolPrusek <karol@DESKTOP-B5Q0PDK>
2023-06-16 18:52:55 +0200

    Promises

--- src/App/Exercises/Js/Exercise-js-promises/index.jsx
+++ src/App/Exercises/Js/Exercise-js-promises/index.jsx
@@ -3,20 +3,54 @@ import './styles.css';
 import { useState } from 'react';
 
 export function Exercise({ parameterToEffect }) {
-  const [getState, setState] = useState(null);
+  // const [getState, setState] = useState(null);
 
-  useEffect(() => {
-    const timer = setInterval(() => {
-      console.log('Wykonuje operacje cykliczna...');
+  // useEffect(() => {
+  //   const timer = setInterval(() => {
+  //     console.log('Wykonuje operacje cykliczna...');
+  //   }, 5000);
+
+  //   //cleanup function inside useEffect
+  //   return () => {
+  //     clearInterval(timer);
+  //     console.log('Zatrzymaj operacje cykliczna...Wykonuje clean up ');
+  //   };
+  //   //to posible options to kick useEffect and cause rerender of the component
+  // }, [parameterToEffect, getState]);
+
+  // //nowe ćwiczenie:
+
+  function logPromisesState(promise) {
+    promise
+      .then(() => {
+        //success
+        console.log('Stan: fulfilled');
+        console.log('Promise zakonczyny sukcesem');
+      })
+      .catch(() => {
+        //failed
+        console.log('Stan: rejected');
+        console.log('Promise zakonczyny błedem');
+      })
+      .finally(() => {
+        //nevermind, I will do this
+        console.log('Stan: fulfilled/rejected');
+        console.log('Promise zakonczyny.');
+      });
+  }
+
+  const myPromise = new Promise((resolve, rejected) => {
+    setTimeout(() => {
+      const random = Math.random();
+      if (random < 0.5) {
+        resolve();
+      } else {
+        rejected();
+      }
     }, 5000);
+  });
 
-    //cleanup function inside useEffect
-    return () => {
-      clearInterval(timer);
-      console.log('Zatrzymaj operacje cykliczna...Wykonuje clean up ');
-    };
-    //to posible options to kick useEffect and cause rerender of the component
-  }, [parameterToEffect, getState]);
+  logPromisesState(myPromise);
 
   return (
     <div className="playing-with-js-promises">
