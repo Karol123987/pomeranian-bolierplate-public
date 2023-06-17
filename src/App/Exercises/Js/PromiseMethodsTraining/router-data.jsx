b120b9857c091a3512b75332df849798919a7092
KarolPrusek <karol@DESKTOP-B5Q0PDK>
2023-06-17 17:04:08 +0200

    Promise_training

---
+++ src/App/Exercises/Js/PromiseMethodsTraining/router-data.jsx
@@ -0,0 +1,10 @@
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'promise-methods-training',
+  date: '17.06.2023',
+  linkLabel: 'Promise methods ćwiczenie',
+  blockNo: 35,
+  element: <Exercise />,
+  tags: ['promise', 'methods', 'all', 'allSettled', 'any', 'race'],
+};
