a60547da6dfeb22a0634fe83821baf0874455572
KarolPrusek <karol@DESKTOP-B5Q0PDK>
2023-06-18 11:20:30 +0200

    ServerExercise/18.06.2023

---
+++ src/App/Exercises/Js/ToDoWithServer/router-data.jsx
@@ -0,0 +1,10 @@
+import { Exercise } from './index';
+
+export const blockRouterMetaData = {
+  path: 'to-do-with-server',
+  date: '18.06.2023',
+  linkLabel: 'ToDoWithServer',
+  blockNo: 35,
+  element: <Exercise />,
+  tags: ['todo', 'fetch', 'rest', 'api', 'javascript'],
+};
