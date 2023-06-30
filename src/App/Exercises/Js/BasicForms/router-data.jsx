811019e447513e2da793c453ddea7979fdd7f633
KarolPrusek <karol@DESKTOP-B5Q0PDK>
2023-06-30 21:11:20 +0200

    BasicForms/part1/30.06.2023

---
+++ src/App/Exercises/Js/BasicForms/router-data.jsx
@@ -0,0 +1,10 @@
+import { Exercise } from "./index";
+
+export const blockRouterMetaData = {
+  path: "basic-forms",
+  date: "30-06-2023",
+  linkLabel: "Basic Forms",
+  blockNo: 43,
+  element: <Exercise />,
+  tags: ["forms", "form", "formularz", "input", "checkbox", "textarea"],
+};
